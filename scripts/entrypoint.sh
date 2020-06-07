6a1bfc3d8e5949bc3c7be3b3c809aa5ef1a7b198
saltbo <saltbo@foxmail.com>
2020-06-07 23:18:44 +0800

    chore: update the entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,7 @@ ARGS="--driver ${INPUT_DRIVER} --region ${INPUT_REGION} --bucket ${INPUT_BUCKET}
 if [ "$INPUT_EXCLUDE" ]; then
   ARGS+=" --exclude $INPUT_EXCLUDE"
 fi
+echo "${ARGS} ${INPUT_DIST}"
 
 # shellcheck disable=SC2086
 uptoc ${ARGS} ${INPUT_DIST}
