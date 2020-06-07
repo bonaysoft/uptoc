b658375802d868c3a9b98f26b4d014c40c6ffce3
saltbo <saltbo@foxmail.com>
2020-06-07 23:09:12 +0800

    fix: update the entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,6 @@ ARGS="--driver ${INPUT_DRIVER} --region ${INPUT_REGION} --bucket ${INPUT_BUCKET}
 if [ "$INPUT_EXCLUDE" ]; then
   ARGS+=" --exclude $INPUT_EXCLUDE"
 fi
-ARGS+=" $INPUT_DIST"
 
 # shellcheck disable=SC2086
-uptoc ${ARGS}
+uptoc ${ARGS} ${INPUT_DIST}
