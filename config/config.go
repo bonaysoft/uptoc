4d6fddddd8d9f6f50293908a9f420248b5b858c5
Ambor <saltbo@foxmail.com>
2020-06-28 23:12:10 +0800

    fix: error config parse (#17)

--- config/config.go
+++ config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -17,59 +16,35 @@ import (
 
 // Config provides yml configuration.
 type Config struct {
-	f *os.File
-
 	Core   engine.Config   `yaml:"core"`
 	Driver uploader.Config `yaml:"driver"`
 }
 
-// Parse returns Config from RC file if no flags and
-// returns Config from flags if any flags exist.
-func Parse(ctx *cli.Context) (*Config, error) {
-	if ctx.NumFlags() > 0 {
-		c := &Config{
-			Core: engine.Config{
-				SaveRoot:  ctx.String(uploaderFlagSaveRoot),
-				ForceSync: true,
-			},
-			Driver: uploader.Config{
-				Name:      ctx.String(uploaderFlagDriver),
-				Region:    ctx.String(uploaderFlagRegion),
-				Bucket:    ctx.String(uploaderFlagBucket),
-				AccessKey: ctx.String(uploaderFlagAccessKey),
-				SecretKey: ctx.String(uploaderFlagSecretKey),
-			},
-		}
-		exclude := ctx.String(uploaderFlagExclude)
-		if exclude != "" {
-			c.Core.Excludes = strings.Split(exclude, ",")
-		}
-
-		return c, nil
-	}
-
-	return ParseFromRC()
+func New() *Config {
+	return &Config{}
 }
 
-// ParseFromRC returns Config from rc file
-func ParseFromRC() (*Config, error) {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
+// Parse returns Config from RC file if no flags and
+// returns Config from flags if any flags exist.
+func NewWithCtx(ctx *cli.Context) (*Config, error) {
+	c := New()
+	if ctx.NumFlags() > 0 {
+		c.Core = engine.Config{
+			SaveRoot:  ctx.String(uploaderFlagSaveRoot),
+			ForceSync: true,
+		}
+		c.Driver = uploader.Config{
+			Name:      ctx.String(uploaderFlagDriver),
+			Region:    ctx.String(uploaderFlagRegion),
+			Bucket:    ctx.String(uploaderFlagBucket),
+			AccessKey: ctx.String(uploaderFlagAccessKey),
+			SecretKey: ctx.String(uploaderFlagSecretKey),
+		}
+		if exclude := ctx.String(uploaderFlagExclude); exclude != "" {
+			c.Core.Excludes = strings.Split(exclude, ",")
+		}
+	} else if err := c.Parse(); err != nil {
 		return nil, err
-	}
-
-	rcPath := filepath.Join(homeDir, ".uptocrc")
-	f, err := os.OpenFile(rcPath, os.O_RDWR, 0644)
-	if os.IsNotExist(err) {
-		return nil, fmt.Errorf("please setup your config by run `uptoc config`")
-	} else if err != nil {
-		return nil, fmt.Errorf("open .uptocrc failed: %s", err)
-	}
-
-	c := &Config{f: f}
-	yd := yaml.NewDecoder(f)
-	if err := yd.Decode(c); err != nil {
-		return nil, fmt.Errorf("decode config failed: %s", err)
 	}
 
 	if strings.HasPrefix(c.Core.SaveRoot, "/") {
@@ -81,6 +56,16 @@ func ParseFromRC() (*Config, error) {
 	}
 
 	return c, nil
+}
+
+// Parse returns Config from rc file
+func (c *Config) Parse() error {
+	f, err := c.open(os.O_RDONLY)
+	if err != nil {
+		return err
+	}
+
+	return yaml.NewDecoder(f).Decode(c)
 }
 
 // Prompt implement a prompt for the config
@@ -115,7 +100,27 @@ func (c *Config) Prompt() error {
 		*prompt.value = value
 	}
 
-	defer c.f.Close()
-	c.f.Seek(0, io.SeekStart)
-	return yaml.NewEncoder(c.f).Encode(c)
+	f, err := c.open(os.O_CREATE | os.O_WRONLY)
+	if err != nil {
+		return err
+	}
+
+	return yaml.NewEncoder(f).Encode(c)
+}
+
+func (c *Config) open(flag int) (*os.File, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	rcPath := filepath.Join(homeDir, ".uptocrc")
+	f, err := os.OpenFile(rcPath, flag, 0644)
+	if os.IsNotExist(err) {
+		return nil, fmt.Errorf("please setup your config by run `uptoc config`")
+	} else if err != nil {
+		return nil, fmt.Errorf("open .uptocrc failed: %s", err)
+	}
+
+	return f, err
 }
